e23c8f4e8f6359265afa4860b781a09ab9dcb09e
gitchamila <chamilamadusankha@gmail.com>
2026-07-11 17:32:38 +1200

    fix: update Playwright HTML reporter to always open the report

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,13 +7,15 @@ export default defineConfig({
     timeout: 10_000,
   },
 
+  //repeatEach: 5, 
+
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
 
   reporter: [
     ['list'],
-    ['html', { outputFolder: 'reports', open: 'never' }],
+    ['html', { outputFolder: 'reports', open: 'always' }],
   ],
 
   use: {
